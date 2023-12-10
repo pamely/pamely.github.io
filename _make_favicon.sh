82cbcfd18eb67bb1f3f7f6f226a67dcf5d08ab6d
Pamely ZANTOU <pamelyking@gmail.com>
2023-12-10 13:58:24 +0200

    Update _make_favicon.sh

--- _make_favicon.sh
+++ _make_favicon.sh
@@ -1,1 +1,1 @@
-convert -background transparent "images/pam-favicon.png" -define icon:auto-resize=16,24,32,48,64,72,96,128,256 "favicon.ico"
+convert  "images/pam-favicon.png" -define icon:auto-resize=16,24,32,48,64,72,96,128,256 "favicon.ico"
